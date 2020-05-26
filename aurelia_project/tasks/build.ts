bc6056769da636047de8b9e0961fbc71d3d563d1
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-05-26 14:15:21 +1000

    fix(build): remove process exit

--- aurelia_project/tasks/build.ts
+++ aurelia_project/tasks/build.ts
@@ -3,7 +3,7 @@ import * as webpack from 'webpack';
 import * as project from '../aurelia.json';
 import * as gulp from 'gulp';
 import * as del from 'del';
-import {CLIOptions, Configuration} from 'aurelia-cli';
+import { CLIOptions, Configuration } from 'aurelia-cli';
 import configureEnvironment from './environment';
 
 const analyze = CLIOptions.hasFlag('analyze');
@@ -14,45 +14,45 @@ const extractCss = buildOptions.isApplicable('extractCss');
 const coverage = buildOptions.isApplicable('coverage');
 
 const config = webpackConfig({
-  production, server, extractCss, coverage, analyze
+    production, server, extractCss, coverage, analyze
 });
 const compiler = webpack(<any>config);
 
 function buildWebpack(done) {
-  if (CLIOptions.hasFlag('watch')) {
-    compiler.watch({}, onBuild);
-  } else {
-    compiler.run(onBuild);
-    compiler.hooks.done.tap('done', () => done());
-  }
+    if (CLIOptions.hasFlag('watch')) {
+        compiler.watch({}, onBuild);
+    } else {
+        compiler.run(onBuild);
+        compiler.hooks.done.tap('done', () => done());
+    }
 }
 
 function onBuild(err, stats) {
-  if (!CLIOptions.hasFlag('watch') && err) {
-    console.error(err.stack || err);
-    if (err.details) console.error(err.details);
-    process.exit(1);
-  } else {
-    process.stdout.write(stats.toString({ colors: require('supports-color') }) + '\n');
+    if (!CLIOptions.hasFlag('watch') && err) {
+        console.error(err.stack || err);
+        if (err.details) console.error(err.details);
+        //process.exit(1);
+    } else {
+        process.stdout.write(stats.toString({ colors: require('supports-color') }) + '\n');
 
-    if (!CLIOptions.hasFlag('watch') && stats.hasErrors()) {
-      process.exit(1);
+        if (!CLIOptions.hasFlag('watch') && stats.hasErrors()) {
+            process.exit(1);
+        }
     }
-  }
 }
 
 function clearDist() {
-  return del([config.output.path]);
+    return del([config.output.path]);
 }
 
 const build = gulp.series(
-  clearDist,
-  configureEnvironment,
-  buildWebpack
+    clearDist,
+    configureEnvironment,
+    buildWebpack
 );
 
 export {
-  config,
-  buildWebpack,
-  build as default
+    config,
+    buildWebpack,
+    build as default
 };
