f0ff3217213b34c2e04fdb3b09917ccddda5da0f
lion-200 <i.duz86@outlook.com>
2020-08-15 19:12:58 +0200

    Re-list BLURT

--- aurelia_project/environments/base.ts
+++ aurelia_project/environments/base.ts
@@ -6,7 +6,7 @@ export const baseEnvironmentConfiguration: Partial<EnvironmentInterface> = {
     CONVERTER_API: 'https://converter-api.hive-engine.com/api/',
     FIREBASE_API: 'https://us-central1-tribaldex-d22e0.cloudfunctions.net/api/',
     maintenanceMode: false,
-    disabledTokens: ['BTC', 'LTC', 'HIVE', 'HBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'LASSE', 'TIME', 'R', 'SCTR', 'ALLAH', 'BNB', 'DONE', 'ETHER', 'LTCPEG', 'SBC', 'BEEHIVE', 'BEECASH', 'BLURT'],
+    disabledTokens: ['BTC', 'LTC', 'HIVE', 'HBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'LASSE', 'TIME', 'R', 'SCTR', 'ALLAH', 'BNB', 'DONE', 'ETHER', 'LTCPEG', 'SBC', 'BEEHIVE', 'BEECASH'],
     peggedToken: 'SWAP.HIVE',
     features: {
         nfts: {
